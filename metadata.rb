6e5bc9c25a854beb9ee038be5585102b97538874
Keno Fischer <kfischer@college.harvard.edu>
2012-06-14 19:48:59 +0200

    Added gfortran

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Keno Fischer"
+maintainer_email "kfischer@college.harvard.edu"
+license          "MIT"
+description      "Installs the gfortran package"
+version          "0.1"
+depends "build-essential"
